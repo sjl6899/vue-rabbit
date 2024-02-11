0ef55577bfcd3c2ef3c3e896d3393481d68a0de3
sjl6899 <810330029@qq.com>
2024-02-11 22:24:04 +0800

    scss变量自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 <template>
     <!-- 一级路由出口组件  -->
     <RouterView/>
+    <div class="test">testsss</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test{
+    color: $sucColor;
+}
 </style>
