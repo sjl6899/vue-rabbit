5ea4485840bb20ba13912e16355ddc4c652c3d20
sjl6899 <810330029@qq.com>
2024-02-11 20:50:23 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//测试接口函数
+import {getCategory} from '@/apis/testAPI'
+getCategory().then((result) => {
+    console.log(result);
+}).catch((err) => {
+    
+});
 const app = createApp(App)
 
 app.use(createPinia())
