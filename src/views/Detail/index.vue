f5ad5b61b7e1325ddc1b8f3ebef12d600b12ab6d
sjl6899 <810330029@qq.com>
2024-02-15 16:08:43 +0800

    SKU组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
 import ImageView from '@/components/imageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 const goods = ref({})
 const route = useRoute()
 
@@ -14,6 +15,10 @@ const getGoods = async () => {
 
 onMounted(()=>getGoods())
 
+//sku规格被操作时
+const skuChange=(sku)=>{
+  console.log(sku);
+}
 
 
 </script>
@@ -91,7 +96,7 @@ onMounted(()=>getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
