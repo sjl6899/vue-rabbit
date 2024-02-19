3167bfe3b09f5a468479c4f9a35796820669aa50
sjl6899 <810330029@qq.com>
2024-02-19 14:27:49 +0800

    支付结果展示

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,8 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/PayBack/index.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path 和 component对应关系位置
@@ -31,19 +33,23 @@ const router = createRouter({
           component: SubCategory
         },
         {
-          path:'detail/:id',
-          component:Detail
+          path: 'detail/:id',
+          component: Detail
         },
         {
-          path:'cartlist',
-          component:CartList
+          path: 'cartlist',
+          component: CartList
         },
         {
-          path:'checkout',
-          component:Checkout
-        },{
-          path:'pay',
-          component:Pay
+          path: 'checkout',
+          component: Checkout
+        }, {
+          path: 'pay',
+          component: Pay
+        },
+        {
+          path: 'paycallback',
+          component: PayBack
         }
       ]
     },
